179b1a25f2bbb8da8dce0f80e65f714f758f04c2
HÜMEYRA ÇİMEN <humeyracimen2001@gmail.com>
2024-04-21 13:26:20 +0300

    sending the report by e-mail
    Reporting the data in the student table by e-mail within the specified time

--- Schema.sql
+++ Schema.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS Bolum (
 CREATE TABLE IF NOT EXISTS Ogrenci_sayac (
     sayac INTEGER DEFAULT 0
 );
--- INSERT INTO Ogrenci_sayac (sayac) VALUES (0);
 INSERT INTO Ogrenci_sayac (sayac)
 SELECT 0
 WHERE NOT EXISTS (SELECT 1 FROM Ogrenci_sayac);
